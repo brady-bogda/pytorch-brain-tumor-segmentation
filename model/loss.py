f614271e17564d9e83b7d6245cc09a9fc52c5cae
bbogda <bbogda@umich.edu>
2024-06-03 22:47:03 -0700

    updated loss and metrics

--- model/loss.py
+++ model/loss.py
@@ -1,5 +1,9 @@
 import torch.nn.functional as F
 
 
+def binary_cross_entropy(output, target):
+    return F.binary_cross_entropy(output, target)
+
+
 def nll_loss(output, target):
     return F.nll_loss(output, target)
